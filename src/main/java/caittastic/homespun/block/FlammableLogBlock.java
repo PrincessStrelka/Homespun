44815b1476e58ec99719923f96114d0b128de79c
smoluwu <79610644+smoluwu@users.noreply.github.com>
2023-02-09 00:23:48 +0000

    custom stripping now uses toolaction

--- src/main/java/caittastic/homespun/block/FlammableLogBlock.java
+++ src/main/java/caittastic/homespun/block/FlammableLogBlock.java
@@ -2,19 +2,17 @@ package caittastic.homespun.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.ToolAction;
-import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.common.ToolActions;
 import org.jetbrains.annotations.Nullable;
 
 public class FlammableLogBlock extends RotatedPillarBlock{
-  public FlammableLogBlock(Properties p_55926_){
-    super(p_55926_);
+  public FlammableLogBlock(Properties properties){
+    super(properties);
   }
 
   @Override
@@ -34,24 +32,21 @@ public class FlammableLogBlock extends RotatedPillarBlock{
 
   @Override
   public @Nullable BlockState getToolModifiedState(BlockState state, UseOnContext context, ToolAction toolAction, boolean simulate){
-    if(context.getItemInHand().getItem() instanceof AxeItem){
+    if(toolAction == ToolActions.AXE_STRIP){
       if(state.is(ModBlocks.IRONWOOD_LOG.get())){
-        return returnBlockWithSameAxis(state, ModBlocks.STRIPPED_IRONWOOD_LOG);
+        return ModBlocks.STRIPPED_IRONWOOD_LOG.get().defaultBlockState().setValue(AXIS, state.getValue(AXIS));
       }
       else if(state.is(ModBlocks.IRONWOOD_WOOD.get())){
-        return returnBlockWithSameAxis(state, ModBlocks.STRIPPED_IRONWOOD_WOOD);
+        return ModBlocks.STRIPPED_IRONWOOD_WOOD.get().defaultBlockState().setValue(AXIS, state.getValue(AXIS));
       }
       if(state.is(ModBlocks.OLIVE_LOG.get())){
-        return returnBlockWithSameAxis(state, ModBlocks.STRIPPED_OLIVE_LOG);
+        return ModBlocks.STRIPPED_OLIVE_LOG.get().defaultBlockState().setValue(AXIS, state.getValue(AXIS));
       }
       else if(state.is(ModBlocks.OLIVE_WOOD.get())){
-        return returnBlockWithSameAxis(state, ModBlocks.STRIPPED_OLIVE_WOOD);
+        return ModBlocks.STRIPPED_OLIVE_WOOD.get().defaultBlockState().setValue(AXIS, state.getValue(AXIS));
       }
     }
     return super.getToolModifiedState(state, context, toolAction, simulate);
   }
 
-  private BlockState returnBlockWithSameAxis(BlockState state, RegistryObject<Block> blockToReturn){
-    return blockToReturn.get().defaultBlockState().setValue(AXIS, state.getValue(AXIS));
-  }
 }
