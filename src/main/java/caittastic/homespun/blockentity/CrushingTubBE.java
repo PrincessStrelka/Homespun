d98dec3e7f6401c783aafd8fbeb600ca35f6668c
smoluwu <79610644+smoluwu@users.noreply.github.com>
2023-02-27 21:54:08 +0000

    even more progress with crushing tub fluid rendering

--- src/main/java/caittastic/homespun/blockentity/CrushingTubBE.java
+++ src/main/java/caittastic/homespun/blockentity/CrushingTubBE.java
@@ -33,7 +33,8 @@ import net.minecraftforge.items.ItemStackHandler;
 import java.util.Random;
 
 public class CrushingTubBE extends BlockEntity{
-  private final ItemStackHandler itemHandler = new ItemStackHandler(2){
+  int CRAFT_SLOT = 0;
+  private final ItemStackHandler itemHandler = new ItemStackHandler(1){
     @Override
     protected void onContentsChanged(int slot){
       setChanged();
@@ -109,15 +110,17 @@ public class CrushingTubBE extends BlockEntity{
   }
 
   public ItemStack getInputRenderStack(){
-    return itemHandler.getStackInSlot(1);
+    return itemHandler.getStackInSlot(CRAFT_SLOT);
   }
 
   public int getInputRenderStackSize(){
-    return itemHandler.getStackInSlot(1).getCount();
+    return itemHandler.getStackInSlot(CRAFT_SLOT).getCount();
   }
 
-  public FluidStack getRenderFluid(){
-    return FLUID_TANK.getFluid();
+  public FluidStack getStoredFluidStack(){
+    //return FLUID_TANK.getFluid();
+    return new FluidStack(Fluids.WATER, 4000);
+
   }
 
   //just done on the client to keep things in sync?
@@ -128,17 +131,17 @@ public class CrushingTubBE extends BlockEntity{
   }
 
   public boolean placeItem(Player player, ItemStack stack){
-    ItemStack stackInSlot = itemHandler.getStackInSlot(1);
+    ItemStack stackInSlot = itemHandler.getStackInSlot(CRAFT_SLOT);
     Item internalItem = stackInSlot.getItem();
     if(stackInSlot.isEmpty() || (internalItem == stack.getItem())){
       if(stackInSlot.getCount() + stack.getCount() <= stackInSlot.getMaxStackSize()){
-        itemHandler.insertItem(1, stack, false);
+        itemHandler.insertItem(CRAFT_SLOT, stack, false);
         player.getItemInHand(InteractionHand.MAIN_HAND).setCount(0);
       }
       else{
         stack.setCount(stackInSlot.getMaxStackSize() - stackInSlot.getCount());
         player.getItemInHand(InteractionHand.MAIN_HAND).setCount(player.getItemInHand(InteractionHand.MAIN_HAND).getCount() - (stackInSlot.getMaxStackSize() - stackInSlot.getCount()));
-        itemHandler.insertItem(1, stack, false);
+        itemHandler.insertItem(CRAFT_SLOT, stack, false);
       }
       this.level.gameEvent(GameEvent.BLOCK_CHANGE, this.getBlockPos(), GameEvent.Context.of(player, this.getBlockState()));
       this.setChanged();
@@ -167,12 +170,12 @@ public class CrushingTubBE extends BlockEntity{
     int resultFluidCount = 250;
 
     if(
-            itemHandler.getStackInSlot(1).getItem() == craftItem &&
-            itemHandler.getStackInSlot(1).getCount() >= craftItemCount &&
+            itemHandler.getStackInSlot(CRAFT_SLOT).getItem() == craftItem &&
+            itemHandler.getStackInSlot(CRAFT_SLOT).getCount() >= craftItemCount &&
             FLUID_TANK.getSpace() >= resultFluidCount
     ){
       this.level.playSound(null, this.getBlockPos(), SoundEvents.SLIME_BLOCK_FALL, SoundSource.BLOCKS, 0.5F, rand.nextFloat() * 0.1F + 0.9F);
-      this.itemHandler.extractItem(1, craftItemCount, false);
+      this.itemHandler.extractItem(CRAFT_SLOT, craftItemCount, false);
       dropItems(this.level, this.getBlockPos(), resultItem, resultItemCount);
       this.FLUID_TANK.fill(new FluidStack(resultFluid, resultFluidCount), IFluidHandler.FluidAction.EXECUTE);
     }
--- src/main/java/caittastic/homespun/renderer/CrushingTubBER.java
+++ src/main/java/caittastic/homespun/renderer/CrushingTubBER.java
@@ -1,31 +1,28 @@
 package caittastic.homespun.renderer;
 
 import caittastic.homespun.blockentity.CrushingTubBE;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
-import com.mojang.math.Matrix3f;
 import com.mojang.math.Matrix4f;
 import com.mojang.math.Vector3f;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.Sheets;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
-import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.inventory.InventoryMenu;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LightLayer;
 import net.minecraft.world.level.material.Fluid;
+import net.minecraftforge.client.extensions.common.IClientFluidTypeExtensions;
 import net.minecraftforge.fluids.FluidStack;
 import org.jetbrains.annotations.NotNull;
 
@@ -33,14 +30,14 @@ import java.util.Objects;
 import java.util.Random;
 
 public class CrushingTubBER implements BlockEntityRenderer<CrushingTubBE>{
+  private static final float MIN_WH = 5F / 16F; //Width/height of cutout
+  private static final float MAX_WH = 11F / 16F; //Width/height of cutout
+  private static final float MIN_D = -0.01F / 16F; //Depth
+  private static final float MAX_D = 16.01F / 16F; //Depth
+
   public CrushingTubBER(BlockEntityRendererProvider.Context context){
 
   }
-
-  private static final float MIN_WH =  5F/16F; //Width/height of cutout
-  private static final float MAX_WH = 11F/16F; //Width/height of cutout
-  private static final float MIN_D = -0.01F/16F; //Depth
-  private static final float MAX_D = 16.01F/16F; //Depth
 
   @Override
   public void render(
@@ -50,7 +47,8 @@ public class CrushingTubBER implements BlockEntityRenderer<CrushingTubBE>{
           @NotNull MultiBufferSource bufferSource,
           int packedLight,
           int packedOverlay){
-    ItemRenderer itemRenderer = Minecraft.getInstance().getItemRenderer();
+    Minecraft minecraft = Minecraft.getInstance();
+    ItemRenderer itemRenderer = minecraft.getItemRenderer();
     ItemStack stackToRender = entity.getInputRenderStack();
     float startHeight = 0.03125f;
     float pixHeight = 0.0625f;
@@ -63,6 +61,8 @@ public class CrushingTubBER implements BlockEntityRenderer<CrushingTubBE>{
     poseStack.scale(1f, 1f, 1f);
     poseStack.mulPose(Vector3f.XP.rotationDegrees(90));
 
+    int lightLevel = getLightLevel(Objects.requireNonNull(entity.getLevel()), entity.getBlockPos());
+
     //for every item in the stack to be rendered, move up one pixel and render item
     for(int i = 0; i < entity.getInputRenderStackSize(); i++){
       poseStack.translate(0, 0, -pixHeight);
@@ -70,13 +70,55 @@ public class CrushingTubBER implements BlockEntityRenderer<CrushingTubBE>{
       itemRenderer.renderStatic(
               stackToRender,
               ItemTransforms.TransformType.GUI,
-              getLightLevel(Objects.requireNonNull(entity.getLevel()), entity.getBlockPos()),
+              lightLevel,
               OverlayTexture.NO_OVERLAY,
               poseStack,
               bufferSource,
               1);
     }
     poseStack.popPose();
+
+    // fluid rendering starts here //
+    //get info on type of fluid
+    FluidStack fluidStack = entity.getStoredFluidStack();
+    Fluid fluid = fluidStack.getFluid();
+
+    //fluid brightness info
+    int fluidBrightness = Math.max(lightLevel, fluidStack.getFluid().getFluidType().getLightLevel(fluidStack));
+    int l2 = fluidBrightness >> 0x10 & 0xFFFF;
+    int i3 = fluidBrightness & 0xFFFF;
+
+    //fluid colour tint info
+    IClientFluidTypeExtensions renderProperties = IClientFluidTypeExtensions.of(fluid);
+    int fluidTintColour = renderProperties.getTintColor(fluidStack);
+    float red = (fluidTintColour >> 16 & 0xFF) / 255.0F;
+    float green = (fluidTintColour >> 8 & 0xFF) / 255.0F;
+    float blue = (fluidTintColour & 0xFF) / 255.0F;
+    //float alpha = ((fluidTintColour >> 24) & 0xFF) / 255F;
+    float[] fColour = {red, green, blue};
+
+    //fluid texture info
+    TextureAtlasSprite stillFluidSprite = minecraft.getTextureAtlas(InventoryMenu.BLOCK_ATLAS).apply(renderProperties.getStillTexture(fluidStack));
+    VertexConsumer vertexBuffer = bufferSource.getBuffer(RenderType.text(stillFluidSprite.atlas().location()));
+
+    //fluid render info
+    Matrix4f lastPose = poseStack.last().pose();
+    float fluidY = pixHeight * 8;
+    float fluidStartDrawPixel = pixHeight * 2;
+    float fluidEndDrawPixel = pixHeight * 14;
+    float fluidSpriteU0 = stillFluidSprite.getU0() + (stillFluidSprite.getU1() - stillFluidSprite.getU0()) / 8;
+    float fluidSpriteU1 = stillFluidSprite.getU1() - (stillFluidSprite.getU1() - stillFluidSprite.getU0()) / 8;
+    float fluidSpriteV0 = stillFluidSprite.getV0() + (stillFluidSprite.getV1() - stillFluidSprite.getV0()) / 8;
+    float fluidSpriteV1 = stillFluidSprite.getV1() - (stillFluidSprite.getV1() - stillFluidSprite.getV0()) / 8;
+    //north-west
+    vertexBuffer.vertex(lastPose, fluidStartDrawPixel, fluidY, fluidStartDrawPixel).color(fColour[0], fColour[1], fColour[2], 1).uv(fluidSpriteU0, fluidSpriteV1).uv2(l2, i3).endVertex();
+    //south-west
+    vertexBuffer.vertex(lastPose, fluidStartDrawPixel, fluidY, fluidEndDrawPixel).color(fColour[0], fColour[1], fColour[2], 1).uv(fluidSpriteU0, fluidSpriteV0).uv2(l2, i3).endVertex();
+    //south-east
+    vertexBuffer.vertex(lastPose, fluidEndDrawPixel, fluidY, fluidEndDrawPixel).color(fColour[0], fColour[1], fColour[2], 1).uv(fluidSpriteU1, fluidSpriteV0).uv2(l2, i3).endVertex();
+    //north-east
+    vertexBuffer.vertex(lastPose, fluidEndDrawPixel, fluidY, fluidStartDrawPixel).color(fColour[0], fColour[1], fColour[2], 1).uv(fluidSpriteU1, fluidSpriteV1).uv2(l2, i3).endVertex();
+
   }
 
   private int getLightLevel(Level level, BlockPos pos){
@@ -84,4 +126,5 @@ public class CrushingTubBER implements BlockEntityRenderer<CrushingTubBE>{
     int skyLight = level.getBrightness(LightLayer.SKY, pos);
     return LightTexture.pack(blockLight, skyLight);
   }
+
 }
