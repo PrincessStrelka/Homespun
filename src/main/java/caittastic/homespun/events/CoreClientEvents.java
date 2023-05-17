98f9c8169192823124b63fef221b1b1df2b7c696
smoluwu <79610644+smoluwu@users.noreply.github.com>
2023-05-17 19:47:34 +0100

    cleaned up keyword code. added simplified chinese translation to correct place

--- src/main/java/caittastic/homespun/events/CoreClientEvents.java
+++ src/main/java/caittastic/homespun/events/CoreClientEvents.java
@@ -2,63 +2,50 @@ package caittastic.homespun.events;
 
 import caittastic.homespun.Homespun;
 import net.minecraft.client.resources.language.I18n;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.Style;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RenderTooltipEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-import net.minecraft.network.chat.Component;
-import net.minecraft.network.chat.MutableComponent;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.ItemStack;
-
-import java.util.*;
+import java.util.List;
 
 @Mod.EventBusSubscriber(value = Dist.CLIENT, modid = Homespun.MOD_ID)
 public class CoreClientEvents{
+  public static final Style INVISIBLE_STYLE = Style.EMPTY.withFont(new ResourceLocation(Homespun.MOD_ID, "invisible"));
 
-  public static final Style INVISIBLE_STYLE = Style.EMPTY.withFont(new ResourceLocation(Homespun.MOD_ID, "invis"));
-
-  private CoreClientEvents() {}
+  private CoreClientEvents(){}
 
   @SubscribeEvent
-  public static void handleItemTooltipEvent(ItemTooltipEvent event) {
+  public static void handleItemTooltipEvent(ItemTooltipEvent event){
     List<Component> tooltip = event.getToolTip();
     ItemStack stack = event.getItemStack();
-
-      String keywordKey = stack.getDescriptionId() + ".keyword";
-      if (canLocalize(keywordKey)) {
-        if (tooltip.get(0) instanceof MutableComponent mutable) {
-          mutable.append(getKeywordTextComponent(keywordKey));
-        }
-      }
-
+    String keywordKey = stack.getDescriptionId() + ".keyword";
+    if(canLocalize(keywordKey) && tooltip.get(0) instanceof MutableComponent mutable){
+      mutable.append(GetComponentAsInvisible(keywordKey));
+    }
   }
 
   @SubscribeEvent
-  public static void handleRenderTooltipEvent(RenderTooltipEvent.GatherComponents event) {
-
-    if (event.getTooltipElements().isEmpty()) {
+  public static void handleRenderTooltipEvent(RenderTooltipEvent.GatherComponents event){
+    if(event.getTooltipElements().isEmpty())
       return;
-    }
     event.getTooltipElements().get(0).left().ifPresent((text) -> {
-      if (text instanceof MutableComponent mutable) {
+      if(text instanceof MutableComponent mutable)
         mutable.getSiblings().removeIf(string -> string.getStyle().equals(INVISIBLE_STYLE));
-      }
     });
   }
 
-  public static MutableComponent getKeywordTextComponent(String key) {
-    return getTextComponent(key).withStyle(INVISIBLE_STYLE);
-    //return getTextComponent(key);
+  public static MutableComponent GetComponentAsInvisible(String key){
+    return (canLocalize(key) ? Component.translatable(key) : Component.literal(key)).withStyle(INVISIBLE_STYLE);
   }
-  public static MutableComponent getTextComponent(String key) {
-    //if can localize then return component.translatabble else return component.literal
-    return canLocalize(key) ? Component.translatable(key) : Component.literal(key);
-  }
-  public static boolean canLocalize(String key) {
+
+  public static boolean canLocalize(String key){
     return I18n.exists(key);
   }
 }
