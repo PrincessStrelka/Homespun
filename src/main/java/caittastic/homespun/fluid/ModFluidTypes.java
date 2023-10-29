02a054af3fc27ff86109dd97441fc29f8f35c98e
smoluwu <79610644+smoluwu@users.noreply.github.com>
2023-10-29 17:15:45 +0000

    cleared fluid package of version update errors

--- src/main/java/caittastic/homespun/fluid/ModFluidTypes.java
+++ src/main/java/caittastic/homespun/fluid/ModFluidTypes.java
@@ -1,7 +1,6 @@
 package caittastic.homespun.fluid;
 
 import caittastic.homespun.Homespun;
-import com.mojang.math.Vector3f;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.fluids.FluidType;
 import net.minecraftforge.registries.DeferredRegister;
@@ -9,16 +8,15 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 public class ModFluidTypes{
-  public static final ResourceLocation FLUID_STILL = new ResourceLocation(Homespun.MOD_ID,"block/ironberry_juice_still");
-  public static final ResourceLocation FLUID_FLOWING = new ResourceLocation(Homespun.MOD_ID,"block/ironberry_juice_still");
+  public static final ResourceLocation FLUID_STILL = new ResourceLocation(Homespun.MOD_ID, "block/ironberry_juice_still");
+  public static final ResourceLocation FLUID_FLOWING = new ResourceLocation(Homespun.MOD_ID, "block/ironberry_juice_still");
 
   public static final DeferredRegister<FluidType> FLUID_TYPES = DeferredRegister.create(ForgeRegistries.Keys.FLUID_TYPES, Homespun.MOD_ID);
-
+  public static final RegistryObject<FluidType> IRONBERRY_JUICE_FLUID_TYPE = register("ironberry_juice",
+          FluidType.Properties.create());
 
   private static RegistryObject<FluidType> register(String name, FluidType.Properties properties){
     return FLUID_TYPES.register(name, () -> new BaseFluidType(properties, FLUID_STILL, FLUID_FLOWING));
   }
-  public static final RegistryObject<FluidType> IRONBERRY_JUICE_FLUID_TYPE = register("ironberry_juice",
-          FluidType.Properties.create());
 
 }
