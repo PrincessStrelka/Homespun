f2793e834598d8c39e0e319fb272a5c90c16280f
smoluwu <79610644+smoluwu@users.noreply.github.com>
2023-10-29 17:13:40 +0000

    moidifed ironwood leaf colour

--- src/main/java/caittastic/homespun/colourHandler.java
+++ src/main/java/caittastic/homespun/colourHandler.java
@@ -14,14 +14,12 @@ import net.minecraftforge.fml.common.Mod;
 public class colourHandler{
   @SubscribeEvent
   static void registerBlockColours(RegisterColorHandlersEvent.Block event){
-    event.register((blockState, getter, blockPos, tintIndex) -> getter != null && blockPos != null ? BiomeColors.getAverageFoliageColor(getter, blockPos) : FoliageColor.getDefaultColor(), ModBlocks.IRONWOOD_LEAVES.get());
     event.register((blockState, getter, blockPos, tintIndex) -> getter != null && blockPos != null ? BiomeColors.getAverageFoliageColor(getter, blockPos) : FoliageColor.getDefaultColor(), ModBlocks.OLIVE_LEAVES.get());
   }
 
   @SubscribeEvent
   public static void registerItemColors(RegisterColorHandlersEvent.Item event){
     BlockColors blockColors = event.getBlockColors();
-    event.register((stack, tintIndex) -> {if(stack.getItem() instanceof BlockItem blockItem) return blockColors.getColor(blockItem.getBlock().defaultBlockState(), null, null, tintIndex);return 0xFFFFFF;}, ModBlocks.IRONWOOD_LEAVES.get());
     event.register((stack, tintIndex) -> {if(stack.getItem() instanceof BlockItem blockItem) return blockColors.getColor(blockItem.getBlock().defaultBlockState(), null, null, tintIndex);return 0xFFFFFF;}, ModBlocks.OLIVE_LEAVES.get());
   }
 }
